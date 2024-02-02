ae08f954a9c5e136000a7e8d71bbf09ffb63194c
MaFomedanu <constantinliviudaniel@yahoo.com>
2024-02-02 22:11:51 +0200

    cursJava repository init

---
+++ Sesiunea 1 - 30-1-2024/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // string type
+        System.out.println("Hello world!");
+        // char type
+        System.out.println('S');
+        // int type
+        System.out.println(3);
+
+        // declaring a variable - type, name, value
+        int numarMasini = 10;
+        System.out.println(numarMasini);
+
+    }
+}

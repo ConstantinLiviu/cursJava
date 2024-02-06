cc9039102d301509fdecc85dfc7107ed12b1a284
MaFomedanu <constantinliviudaniel@yahoo.com>
2024-02-06 19:12:35 +0200

    Tema sesiunea 1

--- Teme/src/TemaSesiunea1.java
+++ Teme/src/TemaSesiunea1.java
@@ -1,2 +1,10 @@
 public class TemaSesiunea1 {
+    public static void main(String []args) {
+        int primulNumar = 3;
+        int alDoileaNumar = 4;
+        int alTreileaNumar = 100;
+        String cuvant = "Numar";
+        System.out.println(primulNumar + " " + alDoileaNumar + " " + alTreileaNumar + " " + cuvant);
+
+    }
 }

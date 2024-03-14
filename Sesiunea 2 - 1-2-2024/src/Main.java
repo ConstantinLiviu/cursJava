4c0b24c6d88892cc3d6f08f62d450af485784d4c
MaFomedanu <constantinliviudaniel@yahoo.com>
2024-03-14 20:38:05 +0200

    adaugat sesiunea 2

---
+++ Sesiunea 2 - 1-2-2024/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // In timpul acestei sesiuni am instalat si configurat git/GitHub, am discutat despre branchuri si am exersat add/commit/pull/push.
+        // Ca urmare, acest fisier va prezenta strict aceste 2 commenturi
+    }
+}

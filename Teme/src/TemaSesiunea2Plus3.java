156a0709025b9f7fdd94949c5405b6b423bd5143
MaFomedanu <constantinliviudaniel@yahoo.com>
2024-03-15 22:38:20 +0200

    redenumit fisier
    combinat temele sesiunilor 2 si 3 intr-un fisier: exercitii legate de git/GitHub

---
+++ Teme/src/TemaSesiunea2Plus3.java
@@ -0,0 +1,31 @@
+public class TemaSesiunea2Plus3 {
+    // TEMA SESIUNEA 2
+    // fisier creat pentru exercitii in timpul cursului
+    // 1. crearea unui nou repository în github
+    // Intreg continutul cursului (exercitii+teme) vor fi urcare in repositorul mentionat in timpul cursului;
+    // 2. copierea proiectului local
+    // Efectuat prin comenzi de git
+    // 3. crearea unui nou branch
+    // A fost creat branch-ul "branch-nou"
+    // 4. care este denumirea branch-ului principal?
+    // Branchul principal in acest proiect este main branch
+    // 5. care este comanda prin care putem sa ne reîntoarcem pe branch-ul main?
+    // git checkout main
+
+    // TEMA SESIUNEA 3
+    // 1. crearea unui nou branch.
+    // A fost creat branch-ul "exercitii"
+    // 2. upload a 3 fișiere in repository-ul respectiv.
+    // Au fost urcare fisierele aferente cursurilor si temelor 1,2 si 3
+    // 3. crearea unui pull request.
+    // Efectuat pe GitHub, verificabil in istoricul commiturilor
+    // 4. review+acceptarea pull request-ului anterior.
+    // Efectuate pe GitHub, verificabile in istoricul commiturilor
+    // 5. dați exemple de 3 comenzi de git+explicați ce rol au.
+    // git add <nume fisier/e> -> adauga fisierele specificate in staging area; astfel, ele sunt pregatite pentru commit si pot fi comparate cu (sau intoarse la) starea originala;
+    // git commit -m "<mesaj ce descrie modificarile facute>" -> permite salvarea modificarilor facute in repositorul local; este un pas necesar pentru a urca noua varianta a fisierului intr-un repositor remote
+    // git push -> preia toate modificarile salvate prin git commit si le urca intr-un repositor remote (precum e indicat de programator, in prealabil)
+
+    // BONUS: din slide curs 02 rezolvam cel puțin primele 2 exerciții din rubrica Challenge.
+    // Toate exercitiile din aceasta rubrica au fost rezolvate sub o forma sau alta (repo nou, branch-uri noi, comenzi de git, Guthub Repo) pe parcursul intregului curs
+}
--- Teme/src/TemaSesiunea3.java
+++
@@ -1,3 +0,0 @@
-public class TemaSesiunea3 {
-//    fisier creat pentru exercitii in timpul cursului
-}

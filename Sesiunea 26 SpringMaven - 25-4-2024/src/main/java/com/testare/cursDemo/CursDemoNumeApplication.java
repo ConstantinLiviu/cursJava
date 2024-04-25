cfb6064a8e4f70f5f4cdf9caad1a568166b9ab13
MaFomedanu <constantinliviudaniel@yahoo.com>
2024-04-25 20:47:30 +0300

    Sectiunea 26, Intro to Spring projects

---
+++ Sesiunea 26 SpringMaven - 25-4-2024/src/main/java/com/testare/cursDemo/CursDemoNumeApplication.java
@@ -0,0 +1,13 @@
+package com.testare.cursDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursDemoNumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursDemoNumeApplication.class, args);
+	}
+
+}
---
+++ Sesiunea 26 SpringMaven - 25-4-2024/src/test/java/com/testare/cursDemo/CursDemoNumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testare.cursDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursDemoNumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

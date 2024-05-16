e3133234f758059d190fbd4a101ca917fbd6ba17
MaFomedanu <constantinliviudaniel@yahoo.com>
2024-05-16 21:42:08 +0300

    sesiunea 31 init

---
+++ Sesiunea 31 - 16-5-2024/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+//        -creare tabela noua: Contract.
+//                -5 fields/campuri
+//                -creare endpoint getAllContracts
+//        -creare endpoint getContractById
+
+
+        /*
+        * public class ContractController2 {
+        *
+        * }
+        *
+        *
+        *
+        *
+        *
+        * **/
+    }
+}

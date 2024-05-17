cb2704c1fe3d8e98d062c8838878706959ba20fd
MaFomedanu <constantinliviudaniel@yahoo.com>
2024-05-17 21:53:49 +0300

    sesiunea 32 init

---
+++ Sesiunea 32 - 17-5-2024/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // using Hibernate create a table "Car" with 5 fields. Create an endpoint to add elements in table
+
+        /*
+        {
+            "name": "produs100",
+                "priceWithoutTVA": 310,
+                "priceWithTVA": 300,
+                "tva": 10
+        }
+
+         */
+
+    }
+}
